117272d4dbf6a0e9a396d5e8a4357ea742444ebf
Skorpion-610 <124805481+Skorpion-610@users.noreply.github.com>
2023-03-01 11:11:04 +0100

    Add files via upload

--- script_meteo.sh
+++ script_meteo.sh
@@ -8,16 +8,16 @@ scanner = Scanner()
 print("Début du scan, c'est parti !")
 while True:
     devices = scanner.scan(timeout=3.0)
-
     for device in devices:
-        print(
-            f"Appareil trouvé ! Adresse MAC : {device.addr}, "
-            f"Puissance du signal : {device.rssi} dB"
-        )
-        print()
-        for adtype, description, value in device.getScanData():
-            print(f"Numéro de profil générique :  ({adtype}), Description : {description} = {value}")
-        print()
-        print()
+        mac_adresses = 'd6:1c:bf:b7:76:62', 'd6:c6:c7:39:a2:e8', 'd7:ef:13:27:15:29'
+        if device.addr in mac_adresses:        
+            print(
+                f"Appareil trouvé ! Adresse MAC : {device.addr}, "
+                f"Puissance du signal : {device.rssi} dB"
+            )
+            print()
+            for adtype, description, value in device.getScanData():
+                print(f"Numéro de profil générique :  ({adtype}), Description : {description} = {value}")
+            print()
 EOF
 sudo python meteo_bluetooth.py
