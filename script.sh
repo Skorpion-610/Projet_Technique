8354ba31a1b66c4e40fc61c14d5215a85a9a955a
Skorpion-610 <124805481+Skorpion-610@users.noreply.github.com>
2023-03-08 14:44:56 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -5,19 +5,32 @@ git clone https://github.com/Skorpion-610/Projet_Technique.git
 sudo apt-get update
 sudo apt-get install ntp -y
 sudo service ntp start
+sudo systemctl enable ssh.service
 
 # Partie création filezilla
 sudo apt-get install vsftpd
 sudo cp Projet_Technique/vsftpd.conf /etc/vsftpd.conf
 sudo cp Projet_Technique/vsftpd.chroot_list /etc/vsftpd.chroot_list
 
+# Challenge 4
+
+pip install peewee
+sudo apt install mariadb-server-10.0 -y
+sudo mysql -u root -e "CREATE DATABASE TPFRANCKTHOMAS;" -e "GRANT ALL PRIVILEGES ON TPFRANCKTHOMAS.* TO fp IDENTIFIED BY 'fp';"
+sudo python donnees.py &
+
 # Challenge 2-3
 
 #!/bin/bash
 sudo apt-get install -y python3-pip libglib2.0-dev
 sudo pip install bluepy
+sudo pip install mysql.connector
+
 cat > meteo_bluetooth.py << EOF
 from bluepy.btle import Scanner
+
+import mysql.connector
+import os
 
 scanner = Scanner()
 print("Début du scan, c'est parti !")
@@ -45,10 +58,72 @@ while True:
                     decimal_int = int(humidite, 16)
                     print(f"L'humidité est de",decimal_int*10**-2,"%")
             print()
+
+utilisateur = os.getlogin();
+# Connection à la base de données MySQL
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="fp",
+    password="fp",
+    database=" TPFRANCKTHOMAS"
+)
+
+cur = conn.cursor()
+
+# Création d'une table si elle n'existe pas
+cur.execute('''CREATE TABLE IF NOT EXISTS TPFRANCKTHOMAS (
+               id INT AUTO_INCREMENT PRIMARY KEY,
+               batterie VARCHAR(255),
+               temperature VARCHAR(255),
+               humidite VARCHAR(255)
+            )''')
+
+# Insérer des données dans la table
+batteries = batterie
+temperatures= temperature
+humidites= humidite
+cur.execute("INSERT INTO TPFRANCKTHOMAS (batteries, temperatures, humidites) VALUES (%s, %s, %s)", (batteries, temperatures, humidites))
+
+# Enregistrer les modifications
+conn.commit()
+
 EOF
 sudo python meteo_bluetooth.py &
 
-# Challenge 4
+# challenge 5 
 
-sudo apt install mariadb-server-10.0 -y
-sudo mysql -u root -e "CREATE DATABASE TPFRANCKTHOMAS;" -e "GRANT ALL PRIVILEGES ON TPFRANCKTHOMAS.* TO fp IDENTIFIED BY 'fp';"
+pip install Flask
+
+mkdir templates
+
+cat > home.html << EOF
+<a href="{{ url_for('redirect_to_capteur') }}" class="btn btn-primary">Afficher les données du capteur</a>
+EOF
+
+sudo cp home.html templates
+
+cat > script_flask.py << EOF
+from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+# Route principale pour afficher la page d'acceuil
+@app.route('/')
+def hello():
+    return render_template("home.html")
+
+# Route secondaire pour afficher la page du capteur
+@app.route('/capteur')
+def capteur():
+    return 'Batterie : / Température : / Humiditée : '
+
+# Bouton pour aller sur la page des informations du capteur
+@app.route("/redirect_to_capteur")
+def redirect_to_capteur():
+    return redirect(url_for("capteur"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+EOF
+sudo python script_flask.py &
