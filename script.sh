a23a0a0b8bbb269e3e82e5f8c78486e74bffe113
Skorpion-610 <124805481+Skorpion-610@users.noreply.github.com>
2023-04-05 11:20:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,22 +10,11 @@ sudo apt install mariadb-server-10.0 -y
 sudo mysql -u root -e "CREATE DATABASE TPFRANCKTHOMAS;" -e "GRANT ALL PRIVILEGES ON TPFRANCKTHOMAS.* TO fp IDENTIFIED BY 'fp';"
 sudo apt-get install -y python3-pip libglib2.0-dev
 sudo pip install bluepy
+sudo pip install pymysql
+sudo pip install Flask
 
 # Exécution du script météo
 sudo python meteo_bluetooth.py &
-
-sudo pip install pymysql
-
-# Exécution du script peewee
-python peeweeTP.py
-
-pip install Flask
-
-mkdir templates
-
-cd ~/Projet_Technique/Flask/
-cp home.html ~/templates
-cp static ~/
 
 # Exécution du script flask
 sudo python script_flask.py &
