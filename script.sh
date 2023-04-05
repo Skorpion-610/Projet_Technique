3a0986eead9193caafa573bb57a7c757ff94db30
Skorpion-610 <124805481+Skorpion-610@users.noreply.github.com>
2023-04-05 11:26:56 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ sudo apt-get install ntp -y
 sudo service ntp start
 sudo systemctl enable ssh.service
 sudo apt-get install vsftpd
-sudo cp Projet_Technique/vsftpd.conf /etc/vsftpd.conf
-sudo cp Projet_Technique/vsftpd.chroot_list /etc/vsftpd.chroot_list
+sudo cp vsftpd.conf /etc/vsftpd.conf
+sudo cp vsftpd.chroot_list /etc/vsftpd.chroot_list
 sudo pip install peewee
 sudo apt install mariadb-server-10.0 -y
 sudo mysql -u root -e "CREATE DATABASE TPFRANCKTHOMAS;" -e "GRANT ALL PRIVILEGES ON TPFRANCKTHOMAS.* TO fp IDENTIFIED BY 'fp';"
